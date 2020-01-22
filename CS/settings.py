d52858ad63a4098fb23a3cc3e62613bd26f4f332
ClauVex <183383@ids.upchiapas.edu.mx>
2020-01-22 11:26:53 -0600

    cliente-servidor2

--- CS/settings.py
+++ CS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mef95zi4vwt&-zwn#(=em^*%cp8$z*n4-mqth-%4$oi980$#o3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
